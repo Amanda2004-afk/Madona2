8576f9dffa2d83af84444178a4dc98397a7a6c1a
amanda <amanda.galdika@inbox.lv>
2021-05-25 11:30:48 +0300

    pabts pd

--- 7uzdevums.py
+++ 7uzdevums.py
@@ -4,3 +4,12 @@ Uzrakstiet programmu Python, lai aprēķinātu
 Ja ievadītās vērtības ir vienādas, atgrieziet summu,
 divkāršojot un paņemot tās absolūto vērtību.
 """
+sk1=float(input("ievadi skaitli:"))
+sk2=float(input("ievadi skaitli:"))
+sk3=float(input("ievadi skaitli:"))
+sk4=float(input("ievadi skaitli:"))
+
+summa=sk1+sk2+sk3+sk4
+if sk1==sk2==sk3==sk4:
+    print(4*summa)
+print(summas)
